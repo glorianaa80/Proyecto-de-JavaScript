6384d03969a33acc3f2c71e6fb0b35dba9c76c2b
Heylin Gutiérrez Torres <h.gutierrez@b7-09.local>
2018-08-13 13:53:05 -0600

    Cambiando header

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // animales
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
     console.log('el contenido ha cargado');
 
     let imagenes = [];
@@ -12,22 +12,22 @@ window.addEventListener('load', function(){
     let indiceImagenes = 0;
     let tiempo = 2000;
 
-    function cambiarImagenes(){
+    function cambiarImagenes() {
 
-         document.galeria.src = imagenes[indiceImagenes];
+        document.galeria.src = imagenes[indiceImagenes];
 
         if (indiceImagenes < 3) {
-        indiceImagenes++;
-        }else{
-        indiceImagenes = 0;
+            indiceImagenes++;
+        } else {
+            indiceImagenes = 0;
         }
     }
 
-    setInterval(cambiarImagenes , tiempo);
+    setInterval(cambiarImagenes, tiempo);
 });
 
 // Monumentos
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
     console.log('el contenido ha cargado');
 
     let imagenes2 = [];
@@ -40,26 +40,26 @@ window.addEventListener('load', function(){
     let indiceImagenes2 = 0;
     let tiempo2 = 2000;
 
-    function cambiarImagenes2(){
+    function cambiarImagenes2() {
 
-         document.galeria2.src = imagenes2[indiceImagenes2];
+        document.galeria2.src = imagenes2[indiceImagenes2];
 
         if (indiceImagenes2 < 3) {
-        indiceImagenes2++;
-        }else{
-        indiceImagenes2 = 0;
+            indiceImagenes2++;
+        } else {
+            indiceImagenes2 = 0;
         }
     }
 
-    setInterval(cambiarImagenes2 , tiempo2);
+    setInterval(cambiarImagenes2, tiempo2);
 });
 // blanco y negro
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
     console.log('el contenido ha cargado');
 
     let imagenes3 = [];
 
-    
+
     imagenes3[0] = 'img/bn1.jpg';
     imagenes3[1] = 'img/bn2.jpg';
     imagenes3[2] = 'img/bn3.jpg';
@@ -68,27 +68,27 @@ window.addEventListener('load', function(){
     let indiceImagenes3 = 0;
     let tiempo3 = 2000;
 
-    function cambiarImagenes3(){
+    function cambiarImagenes3() {
 
-         document.galeria3.src = imagenes3[indiceImagenes3];
+        document.galeria3.src = imagenes3[indiceImagenes3];
 
         if (indiceImagenes3 < 3) {
-        indiceImagenes3++;
-        }else{
-        indiceImagenes3 = 0;
+            indiceImagenes3++;
+        } else {
+            indiceImagenes3 = 0;
         }
     }
 
-    setInterval(cambiarImagenes3 , tiempo3);
+    setInterval(cambiarImagenes3, tiempo3);
 });
 
 // Flores
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
     console.log('el contenido ha cargado');
 
     let imagenes4 = [];
 
-    
+
     imagenes4[0] = 'img/flores-1.jpg';
     imagenes4[1] = 'img/flores-2.jpg';
     imagenes4[2] = 'img/flores-3.jpg';
@@ -97,27 +97,27 @@ window.addEventListener('load', function(){
     let indiceImagenes4 = 0;
     let tiempo4 = 2000;
 
-    function cambiarImagenes4(){
+    function cambiarImagenes4() {
 
-         document.galeria4.src = imagenes4[indiceImagenes4];
+        document.galeria4.src = imagenes4[indiceImagenes4];
 
         if (indiceImagenes4 < 3) {
-        indiceImagenes4++;
-        }else{
-        indiceImagenes4 = 0;
+            indiceImagenes4++;
+        } else {
+            indiceImagenes4 = 0;
         }
     }
 
-    setInterval(cambiarImagenes4 , tiempo4);
+    setInterval(cambiarImagenes4, tiempo4);
 });
 
 // Macro
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
     console.log('el contenido ha cargado');
 
     let imagenes5 = [];
 
-    
+
     imagenes5[0] = 'img/macro-1.jpg';
     imagenes5[1] = 'img/macro-2.jpg';
     imagenes5[2] = 'img/macro-3.jpg';
@@ -126,22 +126,22 @@ window.addEventListener('load', function(){
     let indiceImagenes5 = 0;
     let tiempo5 = 2000;
 
-    function cambiarImagenes5(){
+    function cambiarImagenes5() {
 
-         document.galeria5.src = imagenes5[indiceImagenes5];
+        document.galeria5.src = imagenes5[indiceImagenes5];
 
         if (indiceImagenes5 < 3) {
-        indiceImagenes5++;
-        }else{
-        indiceImagenes5 = 0;
+            indiceImagenes5++;
+        } else {
+            indiceImagenes5 = 0;
         }
     }
 
-    setInterval(cambiarImagenes5 , tiempo5);
+    setInterval(cambiarImagenes5, tiempo5);
 });
 
 // Planetas
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
     console.log('el contenido ha cargado');
 
     let imagenes6 = [];
@@ -155,18 +155,18 @@ window.addEventListener('load', function(){
     let indiceImagenes6 = 0;
     let tiempo6 = 2000;
 
-    function cambiarImagenes6(){
+    function cambiarImagenes6() {
 
-         document.galeria6.src = imagenes6[indiceImagenes6];
+        document.galeria6.src = imagenes6[indiceImagenes6];
 
         if (indiceImagenes6 < 3) {
-        indiceImagenes6++;
-        }else{
-        indiceImagenes6 = 0;
+            indiceImagenes6++;
+        } else {
+            indiceImagenes6 = 0;
         }
     }
 
-    setInterval(cambiarImagenes6 , tiempo6);
+    setInterval(cambiarImagenes6, tiempo6);
 });
 
 /*BOTONES*/
